d42ca095ff95be57537d3fd9b4225a48a5b39769
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-04 20:13:45 -0300

    criacao do json das perguntas seguindo a interface question.

---
+++ data/constants/questions.ts
@@ -0,0 +1,109 @@
+import { question } from "../model/question";
+
+export const questions: question[] = [
+  {
+    id: 1,
+    enunciation: "Qual é o maior planeta do Sistema Solar?",
+    options: ["Terra", "Marte", "Júpiter", "Saturno"],
+    answear: 2
+  },
+  {
+    id: 2,
+    enunciation: "Qual é a estrela mais próxima da Terra?",
+    options: ["Sirius", "Alpha Centauri", "Sol", "Betelgeuse"],
+    answear: 2
+  },
+  {
+    id: 3,
+    enunciation: "Qual planeta é conhecido como o Planeta Vermelho?",
+    options: ["Vênus", "Marte", "Júpiter", "Netuno"],
+    answear: 1
+  },
+  {
+    id: 4,
+    enunciation: "Quantos planetas existem no Sistema Solar?",
+    options: ["7", "8", "9", "10"],
+    answear: 1
+  },
+  {
+    id: 5,
+    enunciation: "Qual planeta tem um grande sistema de anéis?",
+    options: ["Urano", "Netuno", "Júpiter", "Saturno"],
+    answear: 3
+  },
+  {
+    id: 6,
+    enunciation: "Quem foi o primeiro ser humano a viajar para o espaço?",
+    options: ["Neil Armstrong", "Buzz Aldrin", "Yuri Gagarin", "John Glenn"],
+    answear: 2
+  },
+  {
+    id: 7,
+    enunciation: "Qual é o nome da galáxia onde a Terra está localizada?",
+    options: ["Andrômeda", "Via Láctea", "Sombrero", "Cata-Vento"],
+    answear: 1
+  },
+  {
+    id: 8,
+    enunciation: "Qual é o satélite natural da Terra?",
+    options: ["Lua", "Fobos", "Deimos", "Europa"],
+    answear: 0
+  },
+  {
+    id: 9,
+    enunciation: "Qual planeta tem a maior lua do Sistema Solar?",
+    options: ["Saturno", "Júpiter", "Urano", "Netuno"],
+    answear: 1
+  },
+  {
+    id: 10,
+    enunciation: "O que é um buraco negro?",
+    options: [
+      "Uma estrela em colapso gravitacional",
+      "Um tipo de cometa",
+      "Uma nebulosa em formação",
+      "Um asteroide gigantesco"
+    ],
+    answear: 0
+  },
+  {
+    id: 11,
+    enunciation: "Qual é o planeta mais quente do Sistema Solar?",
+    options: ["Mercúrio", "Vênus", "Marte", "Júpiter"],
+    answear: 1
+  },
+  {
+    id: 12,
+    enunciation: "Quantas luas tem Marte?",
+    options: ["1", "2", "3", "4"],
+    answear: 1
+  },
+  {
+    id: 13,
+    enunciation: "Qual foi a primeira missão tripulada a pousar na Lua?",
+    options: ["Apollo 11", "Apollo 12", "Apollo 13", "Apollo 15"],
+    answear: 0
+  },
+  {
+    id: 14,
+    enunciation: "O que é uma supernova?",
+    options: [
+      "Uma estrela explodindo",
+      "Um planeta recém-formado",
+      "Uma galáxia em fusão",
+      "Um cometa gigante"
+    ],
+    answear: 0
+  },
+  {
+    id: 15,
+    enunciation: "O que são os cinturões de asteroides?",
+    options: [
+      "Faixas de asteroides entre Marte e Júpiter",
+      "Satélites artificiais orbitando a Terra",
+      "Regiões de gás e poeira no espaço",
+      "Anéis ao redor dos planetas gigantes"
+    ],
+    answear: 0
+  }
+];
