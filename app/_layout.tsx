d9d6ca306a565c9ea38ffb8370cdfb0d403b2bde
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-04 20:14:44 -0300

    adicao das fontes.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
 import { Stack } from "expo-router";
+import {useEffect} from 'react';
+
+SplashScreen.preventAutoHideAsync(); //espera as fontes serem carregadas antes de mostrar a aplicação
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "SOLARSPACEDEMO-Regular": require("@/assets/fonts/SOLARSPACEDEMO-Regular.otf"),
+    "SPACEMISSION": require("@/assets/fonts/SPACEMISSION.otf")
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return <Stack />;
 }
