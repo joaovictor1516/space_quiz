91f67260cb5c1eda4ca5dbf8e9aab90bccaff867
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-05 16:33:47 -0300

    padronizacao nos components para que tenham  a primeira letra maiscula.

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,19 @@
+import { QuestionsComponent } from "@/components/questionnaire/QuestionsComponent";
 import { questions } from "@/data/constants/questions";
-import { Logo } from "@/components/templates/logo";
+import { Logo } from "@/components/templates/Logo";
 import { Page } from "@/components/templates/Page";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <Page>
-      <Logo/>
-    
+      <View style={{ gap: 25 }}>
+
+        <Logo/>
+        
+        <QuestionsComponent question={questions[0]}/>
+      
+      </View>
     </Page>
   );
 }
