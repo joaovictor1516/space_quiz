3a994e4236833afccd0df3b862c50d86f9737afd
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-05 21:24:24 -0300

    remocao da importacao Text, pois nao estava sendo usada.

--- components/questionnaire/QuestionsComponent.tsx
+++ components/questionnaire/QuestionsComponent.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { question } from "@/data/model/question";
 import { Statment } from "./Statment";
 
