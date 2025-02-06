60f1024444c51d7f0396729bc78f7eb58a552308
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-06 14:47:17 -0300

    criacao da funcao do funcionamento do questionario.

---
+++ data/hooks/useQuestionnaire.ts
@@ -0,0 +1,44 @@
+import { questions } from "../constants/questions";
+import { question } from "../model/question";
+import { useState } from "react";
+
+export function useQuestionnaire(){
+    const [indexQuestion, setIndexQuestion] = useState(0);
+    const [answears, setAnswears] = useState<Number[]>([]);
+    const [questionsList, setQuestionsList] = useState<question[]>(drawQuestions());
+
+    function drawQuestions():question[]{
+        const newQuestions = [...questions];
+
+        return newQuestions.
+            sort(() => Math.random() - 0.5).
+            splice(0, 10);
+    };
+    
+    return {
+        get question(){
+            return questionsList[indexQuestion];
+        },
+
+        get answear(){
+            return questions.
+                map((question) => question.answear).
+                map((response, index) => response === answears[index] ? 1 : 0);
+        },
+
+        get completed(){
+            return indexQuestion === questionsList.length;
+        },
+
+        toAnswer(answear: number){
+            setAnswears([...answears, answear]);
+            setIndexQuestion(indexQuestion + 1);
+        },
+
+        reset(){
+            setAnswears([]);
+            setIndexQuestion(0);
+            setQuestionsList(drawQuestions());
+        }
+    }
+}
