1bb09ed1142c0c4911578ca71729aa7845e3315c
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-05 21:05:46 -0300

    adicao da estilizacao do componente.

--- components/questionnaire/Statment.tsx
+++ components/questionnaire/Statment.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 export interface StatmentProps{
     statement: string;
@@ -7,6 +8,10 @@ export interface StatmentProps{
 export function Statment(props: StatmentProps){
     return (
         <View style={ styles.container }>
+            <LinearGradient
+                colors={["#000000cc", "#00000055"]}
+                style={styles.background}
+            />
             <Text style={ styles.statement }>{props.statement}</Text>
         </View>
     )
@@ -14,9 +19,25 @@ export function Statment(props: StatmentProps){
 
 const styles = StyleSheet.create({
     container: {
-        alignItems: "center"
+        alignItems: "center",
+        backgroundColor: "#852E9C",
+        padding: 20,
+        marginHorizontal: 20,
+        borderRadius: 10
     },
     statement: {
-        color: "#fff"
+        color: "#fff",
+        textAlign: "center",
+        fontSize: 18,
+        fontWeight: "bold",
+        opacity: 0.8
+    },
+    background: {
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        borderRadius: 10
     }
 });
