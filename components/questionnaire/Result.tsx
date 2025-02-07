3a801b70974cac5f37f9bd20eb4cad6b3537bb12
joaovictor1516 <voliveira.joao30@sempreceub.com>
2025-02-06 21:38:08 -0300

    adicao dos elementos e inicio da estilizacao do componente responsavel pelos resultados.

--- components/questionnaire/Result.tsx
+++ components/questionnaire/Result.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export interface ResultProps{
     totalQuestions: number;
@@ -7,7 +7,32 @@ export interface ResultProps{
 }
 
 export function Result(props: ResultProps){
-    <View>
-        
-    </View>
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.text }>Você acertou: </Text>
+            
+            <Text style={ styles.importantText }>
+                { Math.round(props.total / props.totalQuestions) * 100 }%
+            </Text>
+            
+            <Pressable style={ styles.button } onPress={ props.reset }>
+                <Text style={ styles.text }>Reiniciar</Text>
+            </Pressable>
+        </View>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        gap: 5
+    },
+    importantText: {
+        color: "#fff"
+    },
+    text: {
+        color: "#fff"
+    },
+    button: {
+
+    }
+})
